ffe7769ce48b43adb8f105121b2cbee5c24524c2
Gerald Wuersching <gerald.wuersching@tum.de>
2024-10-18 14:12:47 +0200

    fix tests

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -8,42 +8,42 @@ import os
 class TestGeometry(unittest.TestCase):
 
     def setUp(self) -> None:
+        # get path of test directory
+        file_dir_path = os.path.dirname(os.path.realpath(__file__))
         try:
-            # Local Test side
-            with open(os.path.abspath('reference_path_b.pic'), 'rb') as path_file:
+            # Local
+            with open(os.path.join(file_dir_path, 'test_data/reference_path_b.pic'), 'rb') as path_file:
                 data_set = pickle.load(path_file)
         except OSError as e:
-            # CI Test side
+            # CI
             with open(os.path.abspath('geometry/reference_path_b.pic'), 'rb') as path_file:
                 data_set = pickle.load(path_file)
+
+        # get reference path from test data
         self.reference_path_test = data_set['reference_path']
-        self.curvilinear_coord_sys = clcs.CurvilinearCoordinateSystem(self.reference_path_test)
+        self.curvilinear_coord_sys = clcs.CurvilinearCoordinateSystem(self.reference_path_test,
+                                                                      eps2=0.0,
+                                                                      resample=False)
 
         try:
-            # Local Test side
-            with open(os.path.abspath('reference_path_property.pic'), 'rb') as property_file:
+            # Local
+            with open(os.path.join(file_dir_path, 'test_data/reference_path_b_data_new.pic'), 'rb') as property_file:
                 property_set = pickle.load(property_file)
         except OSError as e:
-            # CI Test side
+            # CI
             with open(os.path.abspath('geometry/reference_path_property.pic'), 'rb') as property_file:
                 property_set = pickle.load(property_file)
-        self.reference = property_set['reference']
-        self.ref_pos = property_set['ref_pos']
-        self.ref_curv = property_set['ref_curv']
-        self.ref_curv_d = property_set['ref_curv_d']
-        self.ref_theta = property_set['ref_theta']
 
-    def test_reference(self):
-        assert np.allclose(self.curvilinear_coord_sys.reference, self.reference)
+        # get reference path properties from data
+        self.ref_pos = property_set['path_length']
+        self.ref_curv = property_set['curvature']
+        self.ref_theta = property_set['orientation']
 
     def test_ref_pos(self):
         assert np.allclose(self.curvilinear_coord_sys.ref_pos, self.ref_pos)
 
     def test_ref_curv(self):
         assert np.allclose(self.curvilinear_coord_sys.ref_curv, self.ref_curv, atol=1e-04)
-
-    def test_ref_curv_d(self):
-        assert np.allclose(self.curvilinear_coord_sys.ref_curv_d, self.ref_curv_d, atol=1e-04)
 
     def test_ref_theta(self):
         assert np.allclose(self.curvilinear_coord_sys.ref_theta, self.ref_theta)
