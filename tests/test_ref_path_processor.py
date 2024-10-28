5c5f4a707ad37864cf9685ff2a303ffbdb813095
Gerald Wuersching <gerald.wuersching@tum.de>
2024-10-28 16:49:00 +0100

    unit test

--- tests/test_ref_path_processor.py
+++ tests/test_ref_path_processor.py
@@ -6,6 +6,7 @@ import pickle
 
 # third party
 import numpy as np
+from fontTools.ttLib.reorderGlyphs import SubTablePath
 
 from commonroad_clcs.ref_path_processing.factory import ProcessorFactory
 from commonroad_clcs.ref_path_processing.implementation import (
@@ -18,10 +19,12 @@ from commonroad_clcs.ref_path_processing.implementation import (
 )
 from commonroad_clcs.config import (
     CLCSParams,
-    ProcessingOption
+    ProcessingOption,
+    ResamplingOption
 )
 
 from commonroad_clcs import util as clcs_util
+from commonroad_clcs.util import compute_curvature_from_polyline, compute_curvature_from_polyline_python
 
 
 class RefPathProcessorTest(unittest.TestCase):
@@ -66,24 +69,50 @@ class RefPathProcessorTest(unittest.TestCase):
         assert np.array_equal(self.ref_path_orig,
                               ref_path_processor.ref_path_original)
 
+    # TODO parameterize different inputs
+    # TODO add adaptive sampling test
+    # TODO test stored original reference path in RefPathProcessor
     def test_resampling_processor(self):
         """Test case for ResamplingProcessor"""
         # set params
         params = CLCSParams(processing_option=ProcessingOption.RESAMPLE)
+        params.resampling.option = ResamplingOption.FIXED
         params.resampling.fixed_step = 1.0
 
         # process reference path
         ref_path_processor, ref_path_new = self._run_processor(params)
 
         # check
-        assert isinstance(ref_path_processor, ResamplingProcessor)
-        assert np.allclose(clcs_util.compute_segment_intervals_from_polyline(ref_path_new),
-                           1.0,
-                           atol=1e-02)
+        self.assertIsInstance(ref_path_processor, ResamplingProcessor)
+        self.assertTrue(
+            np.allclose(clcs_util.compute_segment_intervals_from_polyline(ref_path_new),
+                        1.0,
+                        atol=1e-02)
+        )
 
+    # TODO parameterize different inputs
     def test_curve_subdivision_processor(self):
         """Test case for CurveSubdivisionProcessor"""
-        pass
+        # set params
+        params = CLCSParams(processing_option=ProcessingOption.CURVE_SUBDIVISION)
+        params.subdivision.degree = 2
+        params.resampling.option = ResamplingOption.FIXED
+        params.resampling.fixed_step = 1.0
+
+        # set max curvature
+        curr_max_curv = np.max(compute_curvature_from_polyline(self.ref_path_orig))
+        params.subdivision.max_curvature = 0.75 * curr_max_curv
+
+        # process reference path
+        ref_path_processor, ref_path_new = self._run_processor(params)
+
+        # check
+        self.assertIsInstance(ref_path_processor, CurveSubdivisionProcessor)
+        # check max curvature
+        self.assertLessEqual(
+            np.max(compute_curvature_from_polyline_python(ref_path_new)),
+            params.subdivision.max_curvature
+        )
 
     def test_spline_smoothing_processor(self):
         """Test case for SplineSmoothingProcessor"""
@@ -94,3 +123,4 @@ class RefPathProcessorTest(unittest.TestCase):
         pass
 
 # TODO use @parameterized.expand for test case parameterization
+# TODO create special test config file
