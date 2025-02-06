14e637604bb8627939bff8eb2903290420ce8021
Gerald Wuersching <gerald.wuersching@tum.de>
2025-02-06 10:27:45 +0100

    update minimal example

--- tutorials/clcs_minimal_example.py
+++ tutorials/clcs_minimal_example.py
@@ -1,38 +1,72 @@
-import numpy as np
+import os
+
+from commonroad.common.file_reader import CommonRoadFileReader
+
+from commonroad_route_planner.route_planner import RoutePlanner
+from commonroad_route_planner.reference_path_planner import ReferencePathPlanner
+
 from matplotlib import pyplot as plt
-from commonroad_clcs.pycrccosy import CurvilinearCoordinateSystem
-
-# ref path
-x_coords = np.linspace(0, 5, num=11)
-y_coords = np.zeros(11)
-ref_path = np.column_stack((x_coords, y_coords))
-
-# Ccosy settings
-default_lim = 30.0
-eps = 0.1
-eps2 = 0.3
-method = 1
-
-# construct ccosy and get projection domain polygon
-ccosy_1 = CurvilinearCoordinateSystem(ref_path, default_lim, eps, eps2, method)
-proj_domain_1 = np.asarray(ccosy_1.projection_domain())
-
-# get modified ref path from ccosy
-ref_path_ccosy = np.asarray(ccosy_1.reference_path())
-
-# point conversion
-pt_cart = np.array([1, 10])
-ccosy_1.cartesian_point_inside_projection_domain(pt_cart[0], pt_cart[1])
-pt_curv = ccosy_1.convert_to_curvilinear_coords(pt_cart[0], pt_cart[1])
-pt_proj = ccosy_1.convert_to_cartesian_coords(pt_curv[0], 0.0)
+from commonroad_clcs.clcs import CurvilinearCoordinateSystem
+from commonroad_clcs.ref_path_processing.factory import ProcessorFactory
+from commonroad_clcs.helper.visualization import plot_scenario_and_clcs
+from commonroad_clcs.config import (
+    CLCSParams,
+    ProcessingOption,
+    ResamplingOption
+)
 
 
-# ==== Visualization
-# plot cartesian proj domains
-plt.figure()
-plt.title("Cartesian CCosy 1")
-plt.plot(ref_path_ccosy[:, 0], ref_path_ccosy[:, 1], color='g', marker='x', markersize=5, zorder=19, linewidth=2.0)
-plt.plot(proj_domain_1[:, 0], proj_domain_1[:, 1], zorder=100, marker=".", color='orange')
-plt.plot([pt_cart[0], pt_proj[0]], [pt_cart[1], pt_proj[1]], zorder=100, linewidth=2,
-         marker='x', markersize=9, color='blue')
+# *************** Load scenario
+# scenario name
+scenario_name = 'USA_Peach-2_1_T-1.xml'
+
+# load scenario and planning problem
+file_path = os.path.join(os.getcwd(), scenario_name)
+scenario, planning_problem_set = CommonRoadFileReader(file_path).open()
+planning_problem = list(planning_problem_set.planning_problem_dict.values())[0]
+
+
+# *************** Plan initial reference path
+# plan routes and reference path
+routes = RoutePlanner(scenario.lanelet_network, planning_problem).plan_routes()
+ref_path = ReferencePathPlanner(
+    lanelet_network=scenario.lanelet_network,
+    planning_problem=planning_problem,
+    routes=routes
+).plan_shortest_reference_path().reference_path
+
+
+# *************** Pre-process reference path
+# initialize CLCS params
+params = CLCSParams()
+
+# pre-process reference path: Here we smooth the path using curve subdivision
+params.processing_option = ProcessingOption.CURVE_SUBDIVISION
+params.subdivision.max_curvature = 0.15
+params.resampling.option = ResamplingOption.ADAPTIVE
+ref_path_processor = ProcessorFactory.create_processor(params)
+ref_path = ref_path_processor(ref_path)
+
+
+# *************** Construct CLCS
+curvilinear_cosy = CurvilinearCoordinateSystem(
+    reference_path=ref_path,
+    params=params,
+    preprocess_path=False
+)
+
+
+# *************** Transform exemplary point
+cartesian_pt = [-20.0, 68.0]
+curvilinear_pt = curvilinear_cosy.convert_to_curvilinear_coords(cartesian_pt[0], cartesian_pt[1])
+
+
+
+# *************** Visualize
+# plot scenario with
+plot_scenario_and_clcs(
+    scenario,
+    curvilinear_cosy,
+    proj_domain_plot="full"
+)
 plt.show()
